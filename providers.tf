e29f5dba6028ad3af01c0e5e7344ff72771900ee
LEWIS SAWE <lewisbet9@gmail.com>
2023-09-30 14:04:53 +0000

    #25 bucket name no longer uses random provider for name

--- providers.tf
+++ providers.tf
@@ -7,10 +7,10 @@ terraform {
 #     }
 #   }
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+  #   random = {
+  #     source = "hashicorp/random"
+  #     version = "3.5.1"
+  #   }
     aws = {
       source = "hashicorp/aws"
       version = "5.16.2"
