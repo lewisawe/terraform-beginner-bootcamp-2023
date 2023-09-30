ea80e8449fee5377948f509ea7b0e211bc1efb3f
LEWIS SAWE <lewisbet9@gmail.com>
2023-09-30 23:17:11 +0300

    #27 refactor s3 bucket into the new terrahouse_aws module (#28)

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-# resource "random_string" "bucket_name" {
-#   lower = true
-#   upper = false
-#   length   = 32
-#   special  = false
-# }
+terraform {
+#   cloud {
+#     organization = "lewis-sawe"
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
+#     workspaces {
+#       name = "Terra-House-1"
+#     }
+#   }
 }
 
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
