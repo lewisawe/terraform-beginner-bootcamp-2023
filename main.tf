2ee765acfd0fc6b9b3af3ef398baa870e8a74d90
LEWIS SAWE <lewisbet9@gmail.com>
2023-10-04 10:00:00 +0000

    #44 should be able to run and create terraform block without errors

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+    required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
 #   cloud {
 #     organization = "lewis-sawe"
 
@@ -8,12 +14,32 @@ terraform {
 #   }
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  assets_path = var.assets_path
-  content_version = var.content_version
+provider "terratowns" {
+  endpoint = "http://localhost:4567/api"
+  user_uuid="ebf59b24-adea-4726-8b47-9873b822a05b" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   assets_path = var.assets_path
+#   content_version = var.content_version
+# }
+
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
 }
