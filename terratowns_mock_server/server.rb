06c5f456aece0a5f87a0235aefb2e656d5a97ef3
LEWIS SAWE <lewisbet9@gmail.com>
2023-10-04 13:06:23 +0300

    44 terratowns provider (#46)
    * #44 Setting up Skeleton for custom terraform provider
    
    * #44 should be able to run and create terraform block without errors

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -77,7 +77,7 @@ class TerraTownsMockServer < Sinatra::Base
   end
 
   def x_user_uuid
-    return 'e328f4ab-b99f-421c-84c9-4ccea042c7d1'
+    return 'ebf59b24-adea-4726-8b47-9873b822a05b'
   end
 
   def find_user_by_bearer_token
